df6a857ff911e150b454737b81eb60002ad28e63
Akorede Olawole <akoredeolawole@gmail.com>
2022-11-03 19:25:53 +0100

    quick bugfix

--- src/components/CheersToTheWeekend/Weekend.jsx
+++ src/components/CheersToTheWeekend/Weekend.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Weekend.css'
 import BurnaBoy from '../../assets/burna boy.svg'
 import AdekunleGold from '../../assets/adekunle gold.svg'
-import Koffee from '../../assets/koffee.svg'
+// import Koffee from '../../assets/koffee.svg'
 import MusicCard from "../MusicCard/MusicCard";
 
 
@@ -22,11 +22,11 @@ const Weekend = ({className}) => {
           htext="Daily Vibes 2"
           img={AdekunleGold}
         />
-        <MusicCard
+        {/* <MusicCard
           ptext="Koffee, Chris Brown, WSTRN, Drake, Future, Burna Boy"
           htext="Daily Vibes 3"
           img={Koffee}
-        />
+        /> */}
         <MusicCard
           ptext="Burna Boy, Oxlade, Davido, Tems Wizkid, Tiwa Savage..... "
           htext="Daily Vibes 1"
@@ -37,11 +37,11 @@ const Weekend = ({className}) => {
           htext="Daily Vibes 2"
           img={AdekunleGold}
         />
-        <MusicCard
+        {/* <MusicCard
           ptext="Koffee, Chris Brown, WSTRN, Drake, Future, Burna Boy"
           htext="Daily Vibes 3"
           img={Koffee}
-        />
+        /> */}
         </div>
 
     </div>
